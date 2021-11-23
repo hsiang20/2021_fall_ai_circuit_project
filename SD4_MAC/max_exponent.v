f8532fcd0bf6c7db200d7c673bfca22cf50215cd
hsiang20 <h511168@nehs.hc.edu.tw>
2021-11-23 10:53:08 +0800

    syn files

--- SD4_MAC/max_exponent.v
+++ SD4_MAC/max_exponent.v
@@ -4,14 +4,14 @@ module max_exponent (input [4:0] exp_0, exp_1, exp_2, exp_3, exp_4, exp_5, exp_6
     reg [4:0] exp01, exp23, exp45, exp67, exp0123, exp4567, exp01234567;
     
     always @(*) begin
-        exp01 <= (exp_0 > exp_1) ? exp_0 : exp_1;
-        exp23 <= (exp_2 > exp_3) ? exp_2 : exp_3;
-        exp45 <= (exp_4 > exp_5) ? exp_4 : exp_5;
-        exp67 <= (exp_6 > exp_7) ? exp_6 : exp_7;
-        exp0123 <= (exp01 > exp23) ? exp01 : exp23;
-        exp4567 <= (exp45 > exp67) ? exp45 : exp67;
-        exp01234567 <= (exp0123 > exp4567) ? exp0123 : exp4567;
-        exp_max <= (exp01234567 > exp_8) ? exp01234567 : exp_8;
+        exp01 = (exp_0 > exp_1) ? exp_0 : exp_1;
+        exp23 = (exp_2 > exp_3) ? exp_2 : exp_3;
+        exp45 = (exp_4 > exp_5) ? exp_4 : exp_5;
+        exp67 = (exp_6 > exp_7) ? exp_6 : exp_7;
+        exp0123 = (exp01 > exp23) ? exp01 : exp23;
+        exp4567 = (exp45 > exp67) ? exp45 : exp67;
+        exp01234567 = (exp0123 > exp4567) ? exp0123 : exp4567;
+        exp_max = (exp01234567 > exp_8) ? exp01234567 : exp_8;
     end
     
 endmodule
