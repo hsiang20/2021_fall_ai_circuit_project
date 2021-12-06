6c03fc1d6a569c7bad6e4791e54cd06c4c28674a
hsiang20 <h511168@nehs.hc.edu.tw>
2021-12-07 01:45:49 +0800

    normal faster

--- SD4_MAC/normalization.v
+++ SD4_MAC/normalization.v
@@ -49,135 +49,135 @@ module normalization (input signed [19:0] signed_sum,
         // end
         
         // Leading One Detector 2
-        // leading_one = 0;
-        // if (unsign_sum[19:16] != 4'b0) begin
-        //     if (unsign_sum_tmp[19]) leading_one = 19;
-        //     else if (unsign_sum_tmp[18]) leading_one = 18;
-        //     else if (unsign_sum_tmp[17]) leading_one = 17;
-        //     else leading_one = 16;
-        // end
-        // else if (unsign_sum[15:12] != 4'b0) begin
-        //     if (unsign_sum_tmp[15]) leading_one = 15;
-        //     else if (unsign_sum_tmp[14]) leading_one = 14;
-        //     else if (unsign_sum_tmp[13]) leading_one = 13;
-        //     else leading_one = 12;
-        // end
-        // else if (unsign_sum[11:8] != 4'b0) begin
-        //         if (unsign_sum_tmp[11]) leading_one = 11;
-        //         else if (unsign_sum_tmp[10]) leading_one = 10;
-        //         else if (unsign_sum_tmp[9]) leading_one = 9;
-        //         else leading_one = 8;
-        // end
-        // else if (unsign_sum[7:4] != 4'b0) begin
-        //         if (unsign_sum_tmp[7]) leading_one = 7;
-        //         else if (unsign_sum_tmp[6]) leading_one = 6;
-        //         else if (unsign_sum_tmp[5]) leading_one = 5;
-        //         else leading_one = 4;
-        // end
-        // else begin
-        //         if (unsign_sum_tmp[3]) leading_one = 3;
-        //         else if (unsign_sum_tmp[2]) leading_one = 2;
-        //         else if (unsign_sum_tmp[1]) leading_one = 1;
-        //         else leading_one = 0;
-        // end
-        
-        // Leading One Detector 3
         leading_one = 0;
         if (unsign_sum[19:16] != 4'b0) begin
-            case (unsign_sum[19:16])
-                15 : leading_one = 19;
-                14 : leading_one = 19;
-                13 : leading_one = 19;
-                12 : leading_one = 19;
-                11 : leading_one = 19;
-                10 : leading_one = 19;
-                9  : leading_one = 19; 
-                8  : leading_one = 19;
-                7  : leading_one = 18;
-                6  : leading_one = 18;
-                5  : leading_one = 18;
-                4  : leading_one = 18;
-                3  : leading_one = 17;
-                2  : leading_one = 17;
-                default: leading_one = 16;
-            endcase
+            if (unsign_sum_tmp[19]) leading_one = 19;
+            else if (unsign_sum_tmp[18]) leading_one = 18;
+            else if (unsign_sum_tmp[17]) leading_one = 17;
+            else leading_one = 16;
         end
         else if (unsign_sum[15:12] != 4'b0) begin
-            case (unsign_sum[15:12])
-                15 : leading_one = 15;
-                14 : leading_one = 15;
-                13 : leading_one = 15;
-                12 : leading_one = 15;
-                11 : leading_one = 15;
-                10 : leading_one = 15;
-                9  : leading_one = 15; 
-                8  : leading_one = 15;
-                7  : leading_one = 14;
-                6  : leading_one = 14;
-                5  : leading_one = 14;
-                4  : leading_one = 14;
-                3  : leading_one = 13;
-                2  : leading_one = 13;
-                default: leading_one = 12;
-            endcase
+            if (unsign_sum_tmp[15]) leading_one = 15;
+            else if (unsign_sum_tmp[14]) leading_one = 14;
+            else if (unsign_sum_tmp[13]) leading_one = 13;
+            else leading_one = 12;
         end
         else if (unsign_sum[11:8] != 4'b0) begin
-            case (unsign_sum[11:8])
-                15 : leading_one = 11;
-                14 : leading_one = 11;
-                13 : leading_one = 11;
-                12 : leading_one = 11;
-                11 : leading_one = 11;
-                10 : leading_one = 11;
-                9  : leading_one = 11; 
-                8  : leading_one = 11;
-                7  : leading_one = 10;
-                6  : leading_one = 10;
-                5  : leading_one = 10;
-                4  : leading_one = 10;
-                3  : leading_one = 9;
-                2  : leading_one = 9;
-                default leading_one = 8;
-            endcase
+                if (unsign_sum_tmp[11]) leading_one = 11;
+                else if (unsign_sum_tmp[10]) leading_one = 10;
+                else if (unsign_sum_tmp[9]) leading_one = 9;
+                else leading_one = 8;
         end
         else if (unsign_sum[7:4] != 4'b0) begin
-            case (unsign_sum[7:4])
-                15 : leading_one = 7;
-                14 : leading_one = 7;
-                13 : leading_one = 7;
-                12 : leading_one = 7;
-                11 : leading_one = 7;
-                10 : leading_one = 7;
-                9  : leading_one = 7; 
-                8  : leading_one = 7;
-                7  : leading_one = 6;
-                6  : leading_one = 6;
-                5  : leading_one = 6;
-                4  : leading_one = 6;
-                3  : leading_one = 5;
-                2  : leading_one = 5;
-                default: leading_one = 4;
-            endcase
+                if (unsign_sum_tmp[7]) leading_one = 7;
+                else if (unsign_sum_tmp[6]) leading_one = 6;
+                else if (unsign_sum_tmp[5]) leading_one = 5;
+                else leading_one = 4;
         end
         else begin
-            case (unsign_sum[3:0])
-                15 : leading_one = 3;
-                14 : leading_one = 3;
-                13 : leading_one = 3;
-                12 : leading_one = 3;
-                11 : leading_one = 3;
-                10 : leading_one = 3;
-                9  : leading_one = 3; 
-                8  : leading_one = 3;
-                7  : leading_one = 2;
-                6  : leading_one = 2;
-                5  : leading_one = 2;
-                4  : leading_one = 2;
-                3  : leading_one = 1;
-                2  : leading_one = 1;
-                default: leading_one = 0;
-            endcase
+                if (unsign_sum_tmp[3]) leading_one = 3;
+                else if (unsign_sum_tmp[2]) leading_one = 2;
+                else if (unsign_sum_tmp[1]) leading_one = 1;
+                else leading_one = 0;
         end
+        
+        // Leading One Detector 3
+        // leading_one = 0;
+        // if (unsign_sum[19:16] != 4'b0) begin
+        //     case (unsign_sum[19:16])
+        //         15 : leading_one = 19;
+        //         14 : leading_one = 19;
+        //         13 : leading_one = 19;
+        //         12 : leading_one = 19;
+        //         11 : leading_one = 19;
+        //         10 : leading_one = 19;
+        //         9  : leading_one = 19; 
+        //         8  : leading_one = 19;
+        //         7  : leading_one = 18;
+        //         6  : leading_one = 18;
+        //         5  : leading_one = 18;
+        //         4  : leading_one = 18;
+        //         3  : leading_one = 17;
+        //         2  : leading_one = 17;
+        //         default: leading_one = 16;
+        //     endcase
+        // end
+        // else if (unsign_sum[15:12] != 4'b0) begin
+        //     case (unsign_sum[15:12])
+        //         15 : leading_one = 15;
+        //         14 : leading_one = 15;
+        //         13 : leading_one = 15;
+        //         12 : leading_one = 15;
+        //         11 : leading_one = 15;
+        //         10 : leading_one = 15;
+        //         9  : leading_one = 15; 
+        //         8  : leading_one = 15;
+        //         7  : leading_one = 14;
+        //         6  : leading_one = 14;
+        //         5  : leading_one = 14;
+        //         4  : leading_one = 14;
+        //         3  : leading_one = 13;
+        //         2  : leading_one = 13;
+        //         default: leading_one = 12;
+        //     endcase
+        // end
+        // else if (unsign_sum[11:8] != 4'b0) begin
+        //     case (unsign_sum[11:8])
+        //         15 : leading_one = 11;
+        //         14 : leading_one = 11;
+        //         13 : leading_one = 11;
+        //         12 : leading_one = 11;
+        //         11 : leading_one = 11;
+        //         10 : leading_one = 11;
+        //         9  : leading_one = 11; 
+        //         8  : leading_one = 11;
+        //         7  : leading_one = 10;
+        //         6  : leading_one = 10;
+        //         5  : leading_one = 10;
+        //         4  : leading_one = 10;
+        //         3  : leading_one = 9;
+        //         2  : leading_one = 9;
+        //         default leading_one = 8;
+        //     endcase
+        // end
+        // else if (unsign_sum[7:4] != 4'b0) begin
+        //     case (unsign_sum[7:4])
+        //         15 : leading_one = 7;
+        //         14 : leading_one = 7;
+        //         13 : leading_one = 7;
+        //         12 : leading_one = 7;
+        //         11 : leading_one = 7;
+        //         10 : leading_one = 7;
+        //         9  : leading_one = 7; 
+        //         8  : leading_one = 7;
+        //         7  : leading_one = 6;
+        //         6  : leading_one = 6;
+        //         5  : leading_one = 6;
+        //         4  : leading_one = 6;
+        //         3  : leading_one = 5;
+        //         2  : leading_one = 5;
+        //         default: leading_one = 4;
+        //     endcase
+        // end
+        // else begin
+        //     case (unsign_sum[3:0])
+        //         15 : leading_one = 3;
+        //         14 : leading_one = 3;
+        //         13 : leading_one = 3;
+        //         12 : leading_one = 3;
+        //         11 : leading_one = 3;
+        //         10 : leading_one = 3;
+        //         9  : leading_one = 3; 
+        //         8  : leading_one = 3;
+        //         7  : leading_one = 2;
+        //         6  : leading_one = 2;
+        //         5  : leading_one = 2;
+        //         4  : leading_one = 2;
+        //         3  : leading_one = 1;
+        //         2  : leading_one = 1;
+        //         default: leading_one = 0;
+        //     endcase
+        // end
 
         // LUT-based Shifter
         case (leading_one) 
